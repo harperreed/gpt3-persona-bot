19a777d53a7d722541d87c4c2b7dd9071f2d4ebf
Harper Reed <harper@nata2.org>
2020-07-22 12:00:20 -0500

    added web

---
+++ web.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+import logging
+from persona_bot import persona_bot
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+logging.info("Starting up bot")
+
+persona = "space"
+bot = persona_bot(persona=persona)
+
+@app.route('/get')
+def bot_response(question=None):
+    question = request.args.get("msg")
+    response = bot.ask(question)
+    return response
+
+@app.route('/')
+def chat():
+    # template largely based off of chatterbot python implementation
+    # https://github.com/chamkank/flask-chatterbot
+    return render_template('index.html', name=persona)
+
+
+
+if __name__ == '__main__':
+        app.run(debug=True,host='0.0.0.0')
